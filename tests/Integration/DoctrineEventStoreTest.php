baacb7e06faa9e1e37fe99752d2c0b76dddb9c4e
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2026-06-06 10:55:21 +0200

    TASK: Add mandatory clock argument

--- tests/Integration/DoctrineEventStoreTest.php
+++ tests/Integration/DoctrineEventStoreTest.php
@@ -5,8 +5,6 @@ namespace Neos\EventStore\DoctrineAdapter\Tests\Integration;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Exception as DbalException;
-use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
-use Doctrine\DBAL\Platforms\SqlitePlatform;
 use Neos\EventStore\DoctrineAdapter\DoctrineEventStore;
 use Neos\EventStore\EventStoreInterface;
 use Neos\EventStore\Model\EventStore\StatusType;
@@ -44,7 +42,7 @@ final class DoctrineEventStoreTest extends AbstractEventStoreTestBase
     public function test_setup_throws_exception_if_database_connection_fails(): void
     {
         $connection = DriverManager::getConnection(['url' => 'mysql://invalid-connection']);
-        $eventStore = new DoctrineEventStore($connection, self::eventTableName());
+        $eventStore = new DoctrineEventStore($connection, self::eventTableName(), EventStoreFakeClock::get());
 
         $this->expectException(DbalException::class);
         $eventStore->setup();
@@ -53,21 +51,21 @@ final class DoctrineEventStoreTest extends AbstractEventStoreTestBase
     public function test_status_returns_error_status_if_database_connection_fails(): void
     {
         $connection = DriverManager::getConnection(['url' => 'mysql://invalid-connection']);
-        $eventStore = new DoctrineEventStore($connection, self::eventTableName());
+        $eventStore = new DoctrineEventStore($connection, self::eventTableName(), EventStoreFakeClock::get());
         self::assertSame($eventStore->status()->type, StatusType::ERROR);
     }
 
     public function test_status_returns_setup_required_status_if_event_table_is_missing(): void
     {
         $connection = DriverManager::getConnection(['url' => 'sqlite:///:memory:']);
-        $eventStore = new DoctrineEventStore($connection, self::eventTableName());
+        $eventStore = new DoctrineEventStore($connection, self::eventTableName(), EventStoreFakeClock::get());
         self::assertSame($eventStore->status()->type, StatusType::SETUP_REQUIRED);
     }
 
     public function test_status_returns_setup_required_status_if_event_table_requires_update(): void
     {
         $connection = self::connection();
-        $eventStore = new DoctrineEventStore($connection, self::eventTableName());
+        $eventStore = new DoctrineEventStore($connection, self::eventTableName(), EventStoreFakeClock::get());
         $eventStore->setup();
         $connection->executeStatement('ALTER TABLE ' . self::eventTableName() . ' RENAME COLUMN metadata TO metadata_renamed');
         self::assertSame($eventStore->status()->type, StatusType::SETUP_REQUIRED);
@@ -76,7 +74,7 @@ final class DoctrineEventStoreTest extends AbstractEventStoreTestBase
     public function test_status_returns_ok_status_if_event_table_is_up_to_date(): void
     {
         $connection = self::connection();
-        $eventStore = new DoctrineEventStore($connection, self::eventTableName());
+        $eventStore = new DoctrineEventStore($connection, self::eventTableName(), EventStoreFakeClock::get());
         $eventStore->setup();
         self::assertSame($eventStore->status()->type, StatusType::OK);
     }
